a42c1dec4fa56059b4961feae8a6fb1315204e86
Souf1an3 <irbouhsoufiane@gmail.com>
2021-05-05 14:30:13 +0200

    Fixed movement for every enemy

--- src/main/java/com/codecool/dungeoncrawl/logic/GameMap.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/GameMap.java
@@ -63,6 +63,7 @@ public class GameMap {
      *     */
     public void updateActor() {
         for (Actor enemy : enemyList) {
+            System.out.println(enemy.getName());
             enemy.onUpdate();
         }
     }
