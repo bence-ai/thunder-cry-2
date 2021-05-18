9667562780596e8122e1035498c8f2b39c0b8779
hubbabubb <gergohuber@gmail.com>
2021-05-18 13:24:37 +0200

    PSQL schema FIX

--- sample_data/schema_ddl.sql
+++ sample_data/schema_ddl.sql
@@ -18,18 +18,6 @@ CREATE TABLE public.player (
     defense integer NOT NULL
 );
 
--- DROP TABLE IF EXISTS public.spell;
--- CREATE TABLE public.spell (
---   id serial NOT NULL PRIMARY KEY,
---   name text NOT NULL
--- );
---
--- DROP TABLE IF EXISTS public.player_spell;
--- CREATE TABLE public.player_spell (
---   player_id integer NOT NULL ,
---   spell_id integer NOT NULL
--- );
-
 INSERT INTO game_state
 VALUES (DEFAULT, 'test', 2, '1976-06-22 19:10:25-07', 1);
 
@@ -38,20 +26,3 @@ VALUES (DEFAULT, 'Test_Hero', 'BROWN_BOY', 180, 350, 'SWORD', 30);
 
 ALTER TABLE ONLY public.game_state
     ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id);
-
--- ALTER TABLE ONLY public.player_spell
---     ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id);
---
--- ALTER TABLE ONLY public.player_spell
---     ADD CONSTRAINT fk_spell_id FOREIGN KEY (spell_id) REFERENCES public.spell(id);
---
--- INSERT INTO spell VALUES (DEFAULT, 'Fire');
--- INSERT INTO spell VALUES (DEFAULT, 'Thunder');
--- INSERT INTO spell VALUES (DEFAULT, 'Blizzard');
--- INSERT INTO spell VALUES (DEFAULT, 'Meteor');
--- INSERT INTO spell VALUES (DEFAULT, 'Flesh');
--- INSERT INTO spell VALUES (DEFAULT, 'Greater');
--- INSERT INTO spell VALUES (DEFAULT, 'Zombie');
--- INSERT INTO spell VALUES (DEFAULT, 'Shadow');
--- INSERT INTO spell VALUES (DEFAULT, 'Curse');
-
