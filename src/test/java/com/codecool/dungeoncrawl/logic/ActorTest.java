d4674051bbb304c6e28b43e0c9068fce40a39044
Souf1an3 <irbouhsoufiane@gmail.com>
2021-05-20 08:53:33 +0200

    Commit before pull, no real changes

--- src/test/java/com/codecool/dungeoncrawl/logic/ActorTest.java
+++ src/test/java/com/codecool/dungeoncrawl/logic/ActorTest.java
@@ -1,26 +1,14 @@
 package com.codecool.dungeoncrawl.logic;
 
 import com.codecool.dungeoncrawl.logic.actors.Player;
+import com.codecool.dungeoncrawl.logic.actors.PlayerAvatar;
 import com.codecool.dungeoncrawl.logic.actors.Skeleton;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxRobot;
 import org.testfx.assertions.api.Assertions;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.ApplicationTest;
-import org.testfx.framework.junit5.Start;
 
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-
-import javafx.application.Application;
-import javafx.scene.control.Button;
-import javafx.stage.Stage;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -31,19 +19,19 @@ class ActorTest extends ApplicationTest {
 
     @Test
     void moveUpdatesCells() {
-        Player player = new Player(gameMap.getCell(1, 1), "Lajos");
+        Player player = new Player(gameMap.getCell(1, 1), "Lajos", PlayerAvatar.BLUE_BOY);
         player.move(1, 0);
 
         assertEquals(2, player.getX());
         assertEquals(1, player.getY());
-        assertEquals(null, gameMap.getCell(1, 1).getActor());
+        assertNull(gameMap.getCell(1, 1).getActor());
         assertEquals(player, gameMap.getCell(2, 1).getActor());
     }
 
     @Test
     void cannotMoveIntoWall() {
         gameMap.getCell(2, 1).setType(CellType.WALL);
-        Player player = new Player(gameMap.getCell(1, 1) ,"Lajos");
+        Player player = new Player(gameMap.getCell(1, 1) ,"Lajos", PlayerAvatar.BLUE_BOY);
 
         player.move(1, 0);
 
@@ -53,7 +41,7 @@ class ActorTest extends ApplicationTest {
 
     @Test
     void cannotMoveOutOfMap() {
-        Player player = new Player(gameMap.getCell(2, 1), "Lajos");
+        Player player = new Player(gameMap.getCell(2, 1), "Lajos", PlayerAvatar.BLUE_GIRL);
         player.move(1, 0);
 
         assertEquals(2, player.getX());
@@ -62,10 +50,9 @@ class ActorTest extends ApplicationTest {
 
     @Test
     void cannotMoveIntoAnotherActor() {
-        Player player = new Player(gameMap.getCell(1, 1), "Lajos");
+        Player player = new Player(gameMap.getCell(1, 1), "Lajos", PlayerAvatar.BROWN_BOY);
         Skeleton skeleton = new Skeleton(gameMap.getCell(2, 1), "Skeleton");
         player.move(1, 0);
-
         assertEquals(1, player.getX());
         assertEquals(1, player.getY());
         assertEquals(2, skeleton.getX());
